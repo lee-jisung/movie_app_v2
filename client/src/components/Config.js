8b98f04946b01841f8994555b928e446d3780344
leejisung <32594290+lee-jisung@users.noreply.github.com>
2020-08-19 01:11:23 +0900

    remove API_KEY

--- client/src/components/Config.js
+++ client/src/components/Config.js
@@ -3,5 +3,5 @@ export const USER_SERVER = '/api/users';
 
 // the movie db url
 export const API_URL = 'https://api.themoviedb.org/3/';
-export const API_KEY = 'c7376840046814e40e13dc437974844b';
+export const API_KEY = <<api_key_from_movieDB>>;
 export const IMAGE_BASE_URL = 'https://image.tmdb.org/t/p/';
